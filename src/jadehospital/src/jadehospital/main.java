a4caec81b2c5d4ca4f0fb1ee666bb05a9355ad22
k.sivatharan@gmail.com <k.sivatharan@gmail.com@8710e0da-6b7e-7dff-3341-c9d1357be103>
2011-05-13 16:39:01 +0000

    version with generateurpatient

--- src/jadehospital/src/jadehospital/main.java
+++ src/jadehospital/src/jadehospital/main.java
@@ -18,25 +18,18 @@ public class main {
 	public static void main(String[] args) throws StaleProxyException {
 		Runtime rt =Runtime.instance();
 		Profile pf=null;
-		System.out.println("début");
+		System.out.println("debut");
 		try{
 			pf = new ProfileImpl();
 		} catch(Exception e){System.out.println(e.getStackTrace());}
 		
 		AgentContainer mc = rt.createMainContainer(pf);
 		try{
-			AgentController ac = mc.createNewAgent("Pierre", "KBAgent" ,null );
-			
+			AgentController ac = mc.createNewAgent("AgentGenerateurPatient", "jadehospital.AgentGenerateurPatient" ,null );
 			ac.start();
+			
 		}catch(Exception e){System.out.println("#####\n" + e.getMessage() + "\n#####\n");}
 		System.out.println("fini");
-		
-		//jade.wrapper.AgentContainer mc = rt.createMainContainer(pf);
-		//AgentController ac = mc.createNewAgent("Pierre", "Chat.AgentChat" ,new Object[]{"Maurice"} );
-		//AgentController ac2 = mc.createNewAgent("Maurice", "Chat.AgentChat" ,new Object[]{"Pierre"});
-		
-		//ac.start();
-		//ac2.start();
 		
 		
 	}
